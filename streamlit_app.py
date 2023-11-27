a3d0358f6cf42cb2f5681c8cc65da86b933e0d76
craiged99 <97758923+craiged99@users.noreply.github.com>
2023-11-27 22:29:12 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,21 @@
 import streamlit as st
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+# Specify a fixed Chrome browser version
+chrome_version = "94.0.4606.61"  # You can update this to the appropriate version
+
 @st.experimental_singleton
 def get_driver():
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    options = Options()
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
+
+    # Specify the Chrome browser version when creating the driver
+    driver_path = ChromeDriverManager(chrome_version=chrome_version).install()
+    return webdriver.Chrome(service=Service(driver_path), options=options)
 
 options = Options()
 options.add_argument('--disable-gpu')
@@ -17,3 +25,5 @@ driver = get_driver()
 driver.get('http://example.com')
 
 st.code(driver.page_source)
+
+driver.quit()  # Make sure to quit the driver when done
